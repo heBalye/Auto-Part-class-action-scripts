8163a477f0f3a6b38414c8fe9973e6b999f030ef
Daniel Fu <danielfu1993@gmail.com>
2019-12-19 12:53:02 -0600

    update ME_df function

--- Eiligble_table.py
+++ Eiligble_table.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 ME_df = pd.read_excel(
-    "C:\\Users\\FFR0103\\Desktop\\My files\\WORK\\Auto part\\QC process\\F and E Series update 12020219.xlsx",
-    sheet_name="MasterEligible",
+    "C:\\Users\\FFR0103\\Desktop\\My files\\WORK\\Auto part\\QC process\\20191218\\NEW eligible vehicles to add 12.18.2019 per John's list.xlsx",
     dtype=str,
 )
 
+ME_df = ME_df.dropna(how="all")
