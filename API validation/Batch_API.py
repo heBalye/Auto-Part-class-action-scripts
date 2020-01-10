5bbbe46a4c8c229dfa31ed54c0c26215f584313a
Fu <Daniel.Fu@ryan.com>
2020-01-10 15:45:02 -0600

    need to be updated - seprate the chunk size

--- API validation/Batch_API.py
+++ API validation/Batch_API.py
@@ -7,24 +7,34 @@ import sys
 import pandas as pd
 import requests
 
+# get target table
+
+vin_df = pd.read_excel(
+    "C:\\Users\\FFR0103\\Desktop\\My files\\WORK\\Auto part\\VIN API\\Source\\Rexel_01032020 vins.xlsx",
+    dtype=str,
+)
+
 # API URL
 url = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/"
 
-# Create chunk that contains 500 vins
+# Create chunk that contains 10 vins
 Dict = {}
-for i in range(len(vin_df["VIN"]) // 500 + 1):
-    if (i + 1) * 500 < len(vin_df["VIN"]):
+for i in range(len(vin_df["VIN"]) // 10 + 1):
+    if (i + 1) * 10 < len(vin_df["VIN"]):
         Dict["Chunk" + str(i)] = ";".join(
-            vin_df["VIN"][(i + 1) * 500 - 500 : (i + 1) * 500]
+            vin_df["VIN"][(i + 1) * 10 - 10 : (i + 1) * 10]
         )
     else:
-        Dict["Chunk" + str(i)] = ";".join(vin_df["VIN"][(i + 1) * 500 - 500 :])
-    print((i + 1) * 500 - 500, (i + 1) * 500)
-# Chunk list 
-Chunks = [Cunk for Chunk in Dict]
+        Dict["Chunk" + str(i)] = ";".join(vin_df["VIN"][(i + 1) * 10 - 10 :])
+    print((i + 1) * 10 - 10, (i + 1) * 10)
+# Chunk list
+Chunks = [Chunk for Chunk in Dict]
+
 # MainBody
 
-def getValid(chunk):
+
+def getValid(Chunk):
+    url = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/"
     post_fields = {"format": "json", "data": Dict[Chunk]}
     r = requests.post(url, data=post_fields)
     result = json.loads(r.text)
@@ -45,7 +55,8 @@ def getValid(chunk):
     ]
     return df
 
-with concurrent.fures.ThreadPoolExecutor() as exe:
-    results = exe.map(getValid，Chunks)
 
-concat = pd.concat([result for result in results])
+# with concurrent.futures.ThreadPoolExecutor() as exe:
+#     results = exe.map(getValid, Chunks)
+
+# concat = pd.concat([result for result in results])
