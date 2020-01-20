41c4c99c3e654d84a7319ca8ffc6e4747135a14b
Daniel Fu <danielfu1993@gmail.com>
2020-01-20 15:20:26 -0600

    Build unittest for CleanVIN class

---
+++ API validation/test_CleanVIN.py
@@ -0,0 +1,36 @@
+import unittest
+
+import pandas as pd
+
+from CleanVIN import Col_to_str, ValidationProcess
+
+
+# test data
+Test_dir = (
+    r"C:\Users\FFR0103\Desktop\My files\Python Project\Auto Part\API validation\test"
+)
+
+Test_filename = r"\Testcase.xlsx"
+
+test_df = pd.read_excel(Test_dir + Test_filename, dtype=str, header=0,)
+
+# # print(test_df)
+
+
+class TestCleanVIN(unittest.TestCase):
+    def setUp(self):
+        self.test_process = ValidationProcess(Col_to_str(test_df, "VIN", ";"), 4)
+
+    # check the len if match
+    def test_len(self):
+        self.assertEqual(len(self.test_process), len(test_df))
+
+    # Check the VIN from process output is the same as input
+    def test_match(self):
+        VIN_process = list(self.test_process.getAll().iloc[:, 0].values)
+        VIN_df = list(test_df.iloc[:, 0].values)
+        self.assertEqual(VIN_process, VIN_df)
+
+
+if __name__ == "__main__":
+    unittest.main()
